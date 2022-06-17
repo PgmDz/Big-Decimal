a84e925007e0783ac5d022268f6623099727c0c0
PgmDz <carloscaique.com@gmail.com>
2022-06-17 00:55:52 -0300

    Initial commit

---
+++ app/src/main/java/com/pgmd/bigdecimal/MainActivity.java
@@ -0,0 +1,14 @@
+package com.pgmd.bigdecimal;
+
+import android.app.*;
+import android.os.*;
+
+public class MainActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
